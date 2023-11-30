3a5d0fe3f42c0cb4490dd1c2a3334ff7e9634de9
ultcrt <zhang_zhe_yuan@outlook.com>
2023-11-30 17:44:45 +0800

    Add TriangleMesh class base

---
+++ include/data/TriangleMesh.h
@@ -0,0 +1,20 @@
+//
+// Created by ultcrt on 23-11-23.
+//
+
+#ifndef ULTRENDERER_TRIANGLEMESH_H
+#define ULTRENDERER_TRIANGLEMESH_H
+
+#include <vector>
+
+namespace UltRenderer {
+    namespace Data {
+
+        class TriangleMesh {
+        private:
+        };
+
+    } // UltRenderer
+} // Data
+
+#endif //ULTRENDERER_TRIANGLEMESH_H
---
+++ src/data/TriangleMesh.cpp
@@ -0,0 +1,10 @@
+//
+// Created by ultcrt on 23-11-23.
+//
+
+#include "data/TriangleMesh.h"
+
+namespace UltRenderer {
+    namespace Data {
+    } // UltRenderer
+} // Data
