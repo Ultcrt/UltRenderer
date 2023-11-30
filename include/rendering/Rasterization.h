a4383d70c5d510fa00e479bd972f680e61a2a9d2
ultcrt <zhang_zhe_yuan@outlook.com>
2023-11-30 17:52:31 +0800

    Add Rasterization method (Draw lines, bugs exists)

---
+++ include/rendering/Rasterization.h
@@ -0,0 +1,77 @@
+//
+// Created by ultcrt on 23-11-21.
+//
+
+#ifndef ULTRENDERER_RASTERIZATION_H
+#define ULTRENDERER_RASTERIZATION_H
+
+#include "data/Image.h"
+
+namespace UltRenderer {
+    namespace Rendering {
+        namespace Rasterization {
+            template<UltRenderer::Data::ImageFormat FORMAT>
+            void Line(UltRenderer::Data::Image<FORMAT>& img, std::size_t x0, std::size_t y0, std::size_t x1, std::size_t y1, const UltRenderer::Data::Pixel<FORMAT>& pixel) {
+                const auto dX = static_cast<std::size_t>(std::abs(static_cast<double>(x0) - static_cast<double>(x1)));
+                const auto dY = static_cast<std::size_t>(std::abs(static_cast<double>(y0) - static_cast<double>(y1)));
+
+                // Set longer axis as sampling direction
+                bool xIsLonger = dX >= dY;
+
+                std::size_t samples, longOrigin, shortOrigin;
+                std::size_t step;
+                bool shortIsIncreasing = true;
+                if (xIsLonger) {
+                    samples = dX;
+                    if (x0 <= x1) {
+                        longOrigin = x0;
+                        shortOrigin = y0;
+                        shortIsIncreasing = y0 < y1;
+                    }
+                    else {
+                        longOrigin = x1;
+                        shortOrigin = y1;
+                        shortIsIncreasing = y1 < y0;
+                    }
+                    step = dY;
+                }
+                else {
+                    samples = dY;
+                    if (y0 <= y1) {
+                        longOrigin = y0;
+                        shortOrigin = x0;
+                        shortIsIncreasing = x0 < x1;
+                    }
+                    else {
+                        longOrigin = y1;
+                        shortOrigin = x1;
+                        shortIsIncreasing = x1 < x0;
+                    }
+                    step = dX;
+                }
+
+                long long error = 0;
+                std::size_t shortPos = shortOrigin;
+                for (std::size_t sampleIdx = 0; sampleIdx < samples; sampleIdx++) {
+                    const std::size_t longPos = longOrigin + sampleIdx;
+
+                    if (2 * error > static_cast<long long>(samples)) {
+                        shortPos += shortIsIncreasing ? 1 : -1;
+                        error -= static_cast<long long>(samples);
+                    }
+
+                    if (xIsLonger) {
+                        img.set(longPos, shortPos, pixel);
+                    }
+                    else {
+                        img.set(shortPos, longPos, pixel);
+                    }
+
+                    error += static_cast<long long>(step);
+                }
+            }
+        }
+    } // UltRenderer
+} // Rendering
+
+#endif //ULTRENDERER_RASTERIZATION_H
---
+++ src/rasterization/Rasterization.cpp
@@ -0,0 +1,10 @@
+//
+// Created by ultcrt on 23-11-21.
+//
+
+#include "rendering/Rasterization.h"
+
+namespace UltRenderer {
+    namespace Rendering {
+    } // UltRenderer
+} // Rendering
