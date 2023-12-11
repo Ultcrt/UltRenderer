a5479ca889e6845e75719a7e2ef0d94fa7f8a518
Ultcrt <zhang_zhe_yuan@outlook.com>
2023-12-11 14:57:04 +0800

    Implement spatial hash function for Vector3 and Vector2

---
+++ include/utils/Hash.h
@@ -0,0 +1,53 @@
+//
+// Created by ultcrt on 23-12-11.
+//
+
+#ifndef ULTRENDERER_HASH_H
+#define ULTRENDERER_HASH_H
+
+#include <algorithm>
+#include "data/Matrix.h"
+
+namespace UltRenderer {
+    namespace Utils {
+        template <typename T>
+        struct SpatialHash3D {
+        private:
+            std::size_t _n;
+
+        public:
+            explicit SpatialHash3D(std::size_t n);
+            std::size_t operator() (const Data::Vector3<T>& target) const;
+        };
+
+        template <typename T>
+        struct SpatialHash2D {
+        private:
+            std::size_t _n;
+
+        public:
+            explicit SpatialHash2D(std::size_t n);
+            std::size_t operator() (const Data::Vector2<T>& target) const;
+        };
+
+        template<typename T>
+        SpatialHash3D<T>::SpatialHash3D(std::size_t n): _n(n) {}
+
+        template<typename T>
+        std::size_t SpatialHash3D<T>::operator()(const Data::Vector3<T> &target) const {
+            std::size_t h = (target.x() * 92837111) ^ (target.y() * 689287499) ^ (target.z() * 283923481);
+            return h % _n;
+        }
+
+        template<typename T>
+        SpatialHash2D<T>::SpatialHash2D(std::size_t n): _n(n) {}
+
+        template<typename T>
+        std::size_t SpatialHash2D<T>::operator()(const Data::Vector2<T> &target) const {
+            std::size_t h = (target.x() * 92837111) ^ (target.y() * 689287499);
+            return h % _n;
+        }
+    } // Utils
+} // UltRenderer
+
+#endif //ULTRENDERER_HASH_H
---
+++ src/utils/Hash.cpp
@@ -0,0 +1,10 @@
+//
+// Created by ultcrt on 23-12-11.
+//
+
+#include "utils/Hash.h"
+
+namespace UltRenderer {
+    namespace Utils {
+    } // Utils
+} // UltRenderer
