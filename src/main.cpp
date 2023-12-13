bc2a3580eb062636c7d5c065bdc2607e9aa647d2
Ultcrt <zhang_zhe_yuan@outlook.com>
2023-12-13 13:24:59 +0800

    Simple obj drawer (test-use)

--- src/main.cpp
+++ src/main.cpp
@@ -2,22 +2,33 @@
 #include "data/Image.h"
 #include "rendering/Rasterization.h"
 #include "data/Matrix.h"
+#include "data/TriangleMesh.h"
 
 int main() {
-    UltRenderer::Data::RGBAImage img(1000, 1000);
+    UltRenderer::Data::RGBAImage img(1000, 1500);
 
-    UltRenderer::Rendering::Rasterization::Line(img, 630, 100, 10, 389, {255, 0, 0, 255});
-    UltRenderer::Rendering::Rasterization::Line(img, 10, 789, 120, 100, {255, 255, 0, 255});
-    UltRenderer::Rendering::Rasterization::Line(img, 133, 56, 12, 750, {255, 255, 0, 255});
+    UltRenderer::Data::TriangleMesh model("../data/african_head.obj");
+
+    UltRenderer::Rendering::Rasterization::Line(img, 0, 0, 999, 999, {255, 0, 0, 255});
+
+
+    for (const auto& tri: model.triangles) {
+        for (std::size_t idx = 0; idx < 3; idx++) {
+            UltRenderer::Data::Vector3D vertex0 = model.vertices[tri[idx]];
+            UltRenderer::Data::Vector3D vertex1 = model.vertices[tri[(idx + 1) % 3]];
+            UltRenderer::Data::Vector2S point0 = {
+                    static_cast<std::size_t>((vertex0.x() + 1) * static_cast<double>(img.shape().y() - 1) / 2),
+                    static_cast<std::size_t>((vertex0.y() + 1) * static_cast<double>(img.shape().x() - 1) / 2)
+            };
+            UltRenderer::Data::Vector2S point1 = {
+                    static_cast<std::size_t>((vertex1.x() + 1) * static_cast<double>(img.shape().y() - 1) / 2),
+                    static_cast<std::size_t>((vertex1.y() + 1) * static_cast<double>(img.shape().x() - 1) / 2)
+            };
+            UltRenderer::Rendering::Rasterization::Line(img, point0.x(), point0.y(), point1.x(), point1.y(), {255, 0, 0, 255});
+        }
+    }
+
     img.save("test.tga");
-
-    UltRenderer::Data::Matrix<int, 3, 2> tmp0({1, 2, 3, 4, 5, 6});
-    UltRenderer::Data::Matrix<int, 2, 3> tmp1({1, 2, 3, 4, 5, 6});
-    auto tmp3 = tmp1 * tmp0;
-
-    tmp3(0, 1) = 23;
-
-    std::cout << tmp3(0, 1) << std::endl;
 
     return 0;
 }
