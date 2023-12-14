0bbb643cf161d09811c1cec08630c06c2259cdf9
Ultcrt <zhang_zhe_yuan@outlook.com>
2023-12-14 15:09:33 +0800

    Implement Triangle draw

--- include/rendering/Rasterization.h
+++ include/rendering/Rasterization.h
@@ -5,6 +5,7 @@
 #ifndef ULTRENDERER_RASTERIZATION_H
 #define ULTRENDERER_RASTERIZATION_H
 
+#include <cmath>
 #include "data/Matrix.h"
 #include "data/Image.h"
 
@@ -16,6 +17,9 @@ namespace UltRenderer {
 
             template<UltRenderer::Data::ImageFormat FORMAT>
             void Line(UltRenderer::Data::Image<FORMAT>& img, const Data::Vector2S& p0, const Data::Vector2S& p1, const UltRenderer::Data::Pixel<FORMAT>& pixel);
+
+            template<UltRenderer::Data::ImageFormat FORMAT>
+            void Triangle(UltRenderer::Data::Image<FORMAT>& img, const Data::Vector2S& p0, const Data::Vector2S& p1, const Data::Vector2S& p2, const  UltRenderer::Data::Pixel<FORMAT>& pixel);
 
             template<UltRenderer::Data::ImageFormat FORMAT>
             void Line(UltRenderer::Data::Image<FORMAT> &img, std::size_t x0, std::size_t y0, std::size_t x1,
@@ -83,6 +87,50 @@ namespace UltRenderer {
             void Line(UltRenderer::Data::Image<FORMAT> &img, const Data::Vector2S &p0,
                                      const Data::Vector2S &p1, const UltRenderer::Data::Pixel<FORMAT> &pixel) {
                 Line<FORMAT>(img, p0.x(), p0.y(), p1.x(), p1.y(), pixel);
+            }
+
+            template<UltRenderer::Data::ImageFormat FORMAT>
+            void Triangle(UltRenderer::Data::Image<FORMAT> &img, const Data::Vector2S &p0,
+                                         const Data::Vector2S &p1, const Data::Vector2S &p2,
+                                         const UltRenderer::Data::Pixel<FORMAT> &pixel) {
+                std::vector<Data::Vector2S> points{p0, p1, p2};
+
+                // Sort three point with y, only compare x when ys are equal
+                std::sort(points.begin(), points.end(), [](const Data::Vector2S& a, const Data::Vector2S& b){
+                    if (a.y() == b.y()) {
+                        return a.x() < b.x();
+                    }
+                    else {
+                        return a.y() < b.y();
+                    }
+                });
+
+                // Draw border of triangle
+                Line<FORMAT>(img, points[0], points[1], pixel);
+                Line<FORMAT>(img, points[1], points[2], pixel);
+                Line<FORMAT>(img, points[2], points[0], pixel);
+
+                // Filling
+                if (points[0].y() < points[1].y()) {
+                    double step01 = (static_cast<double>(points[0].x()) - static_cast<double>(points[1].x())) / (static_cast<double>(points[0].y()) - static_cast<double>(points[1].y()));
+                    double step02 = (static_cast<double>(points[0].x()) - static_cast<double>(points[2].x())) / (static_cast<double>(points[0].y()) - static_cast<double>(points[2].y()));
+                    for (std::size_t idx = 1; idx < points[1].y() - points[0].y(); idx++) {
+                        const std::size_t yIdx = points[0].y() + idx;
+                        const std::size_t xIdx01 = std::lround(static_cast<double>(points[0].x()) + step01 * static_cast<double>(idx));
+                        const std::size_t xIdx02 = std::lround(static_cast<double>(points[0].x()) + step02 * static_cast<double>(idx));
+                        Line<FORMAT>(img, xIdx01, yIdx, xIdx02, yIdx, pixel);
+                    }
+
+                    if (points[1].y() < points[2].y()) {
+                        double step12 = (static_cast<double>(points[1].x()) - static_cast<double>(points[2].x())) / (static_cast<double>(points[1].y()) - static_cast<double>(points[2].y()));
+                        for (std::size_t idx = 0; idx < points[2].y() - points[1].y(); idx++) {
+                            const std::size_t yIdx = points[1].y() + idx;
+                            const std::size_t xIdx12 = std::lround(static_cast<double>(points[1].x()) + step12 * static_cast<double>(idx));
+                            const std::size_t xIdx02 = std::lround(static_cast<double>(points[0].x()) + step02 * static_cast<double>(yIdx - points[0].y()));
+                            Line<FORMAT>(img, xIdx12, yIdx, xIdx02, yIdx, pixel);
+                        }
+                    }
+                }
             }
         }
     } // UltRenderer
