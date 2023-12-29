5ec72a85d66ffa952b19e6d14f3294e2491cd090
Ultcrt <zhang_zhe_yuan@outlook.com>
2023-12-29 13:24:33 +0800

    Add members of Camera

--- include/rendering/Camera.h
+++ include/rendering/Camera.h
@@ -6,17 +6,41 @@
 #define ULTRENDERER_CAMERA_H
 
 #include "math/Matrix.h"
+#include "math/Transform.h"
 
 namespace UltRenderer {
     namespace Rendering {
+        enum class FOVType {
+            H, V, D
+        };
+
         class Camera {
         private:
-            std::size_t _width;
-            std::size_t _height;
+            double _width;
+            double _height;
 
-            double _zMin;
-            double _zMax;
+            double _zMin = 0.1;
+            double _zMax = 10;
         public:
+            Math::Transform3D viewMatrix;
+            Math::Transform3D projectionMatrix;
+
+            static Math::Vector2D ConvertFOVToSize(FOVType fovType, double len, bool isWidth = true);
+
+            Camera(double width, FOVType fovType, double zMin=0.1, double zMax=0.1);
+            Camera(double width, double height, double zMin=0.1, double zMax=0.1);
+
+            double width() const;
+            double height() const;
+            double zMin() const;
+            double zMax() const;
+
+            void setWidth(double width);
+            void setHeight(double height);
+            void setZMin(double zMin);
+            void setZMax(double zMax);
+
+            void render(std::size_t width, std::size_t height);
         };
 
     } // Rendering
