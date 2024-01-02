7a2e56309d1e0bb6cf0648d26e4217e15aae8dd8
Ultcrt <zhang_zhe_yuan@outlook.com>
2024-01-02 16:12:10 +0800

    Implement Scene

--- include/rendering/Scene.h
+++ include/rendering/Scene.h
@@ -5,10 +5,28 @@
 #ifndef ULTRENDERER_SCENE_H
 #define ULTRENDERER_SCENE_H
 
+#include <vector>
+#include "data/TriangleMesh.h"
+#include "rendering/Light.h"
+#include "rendering/Camera.h"
+
 namespace UltRenderer {
     namespace Rendering {
-        class Scene {
+        // Tips: Make it possible to access shared_ptr of this
+        class Scene: public std::enable_shared_from_this<Scene> {
+        private:
+            std::vector<std::shared_ptr<Data::TriangleMesh>> _meshes;
+            std::vector<std::shared_ptr<Rendering::Light>>   _lights;
+            std::vector<std::shared_ptr<Rendering::Camera>>  _cameras;
 
+        public:
+            void addMesh(const std::shared_ptr<Data::TriangleMesh>& target);
+            void addLight(const std::shared_ptr<Rendering::Light>& target);
+            void addCamera(const std::shared_ptr<Rendering::Camera>& target);
+
+            [[nodiscard]] std::vector<std::shared_ptr<Data::TriangleMesh>> meshes();
+            [[nodiscard]] std::vector<std::shared_ptr<Rendering::Light>>   lights();
+            [[nodiscard]] std::vector<std::shared_ptr<Rendering::Camera>>  cameras();
         };
     } // Rendering
 } // UltRenderer
--- src/rendering/Scene.cpp
+++ src/rendering/Scene.cpp
@@ -6,5 +6,33 @@
 
 namespace UltRenderer {
     namespace Rendering {
+        void Scene::addMesh(const std::shared_ptr<Data::TriangleMesh> &target) {
+            target->setScene(shared_from_this());
+            _meshes.emplace_back(target);
+        }
+
+        void Scene::addLight(const std::shared_ptr<Rendering::Light> &target) {
+            target->setScene(shared_from_this());
+            _lights.emplace_back(target);
+        }
+
+        void Scene::addCamera(const std::shared_ptr<Rendering::Camera> &target) {
+            target->setScene(shared_from_this());
+            _cameras.emplace_back(target);
+        }
+
+        std::vector<std::shared_ptr<Data::TriangleMesh>> Scene::meshes() {
+            return _meshes;
+        }
+
+        std::vector<std::shared_ptr<Rendering::Light>> Scene::lights() {
+            return _lights;
+        }
+
+        std::vector<std::shared_ptr<Rendering::Camera>> Scene::cameras() {
+            return _cameras;
+        }
+
+
     } // Rendering
 } // UltRenderer
