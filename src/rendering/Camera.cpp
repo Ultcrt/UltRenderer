c2bb95d49447ae5ebba3c5b4ea5849ec804f55a5
Ultcrt <zhang_zhe_yuan@outlook.com>
2024-01-02 19:02:41 +0800

    Implement Camera render function (in progress)

--- src/rendering/Camera.cpp
+++ src/rendering/Camera.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "rendering/Camera.h"
+#include "rendering/Rasterize.h"
 
 namespace UltRenderer {
     namespace Rendering {
@@ -71,6 +72,60 @@ namespace UltRenderer {
         void Camera::setProjectionType(ProjectionType projectionType) {
             _projectionType = projectionType;
             updateProjectionMatrix();
+        }
+
+        Data::Image
+        Camera::render(std::size_t width, std::size_t height, Data::ImageFormat format) const {
+            // Origin is always (0, 0) here, depth is scaled into (0, 1)
+            Math::Transform3D viewport;
+            viewport(0, 0) = static_cast<double>(width) / 2.;
+            viewport(1, 1) = static_cast<double>(height) / 2.;
+            viewport(2, 2) = 1. / 2.;
+            viewport(0, 3) = static_cast<double>(width) / 2.;
+            viewport(1, 3) = static_cast<double>(height) / 2.;
+            viewport(2, 3) = 1. / 2.;
+
+            UltRenderer::Data::Image result(width, height, format);
+            UltRenderer::Data::Image zBuffer(width, height, Data::ImageFormat::GRAY);
+            zBuffer.fill(1);
+
+            // viewport * projection * view
+            const Math::Transform3D constTransform = viewport * projectionMatrix * transformMatrix.inverse();
+            for (const auto& pMesh: _pScene->meshes()) {
+                const auto& pTexture = pMesh->pTexture;
+                const Math::Transform3D overallTransform = constTransform * pMesh->transformMatrix;
+                for (const auto& tri: pMesh->triangles) {
+                    UltRenderer::Math::Vector3D vertex0 = pMesh->vertices[tri[0]];
+                    UltRenderer::Math::Vector3D vertex1 = pMesh->vertices[tri[1]];
+                    UltRenderer::Math::Vector3D vertex2 = pMesh->vertices[tri[2]];
+
+                    UltRenderer::Math::Vector3D transformedVertex0 = (overallTransform * vertex0.toHomogeneousCoordinates(1)).toCartesianCoordinates();
+                    UltRenderer::Math::Vector3D transformedVertex1 = (overallTransform * vertex1.toHomogeneousCoordinates(1)).toCartesianCoordinates();
+                    UltRenderer::Math::Vector3D transformedVertex2 = (overallTransform * vertex2.toHomogeneousCoordinates(1)).toCartesianCoordinates();
+
+                    // TODO: Need to support texture-less mesh
+                    UltRenderer::Math::Vector3D uv0 = pMesh->vertexTextures[tri[0]];
+                    UltRenderer::Math::Vector3D uv1 = pMesh->vertexTextures[tri[1]];
+                    UltRenderer::Math::Vector3D uv2 = pMesh->vertexTextures[tri[2]];
+
+                    Math::Vector3D avgLight;
+                    // TODO: Compute Average light here for simplicity, which is wrong
+                    for (const auto& light: _pScene->lights()) {
+                        avgLight = avgLight + light->position;
+                    }
+                    UltRenderer::Math::Vector3D triangleNormal = ((vertex0 - vertex2).cross(vertex0 - vertex1)).normalized();
+                    double ratio = triangleNormal.dot(avgLight.normalized());
+
+                    if (ratio > 0) {
+                        Math::Vector2S point0 = {static_cast<std::size_t>(std::round(transformedVertex0.x())), static_cast<std::size_t>(std::round(transformedVertex0.y()))};
+                        Math::Vector2S point1 = {static_cast<std::size_t>(std::round(transformedVertex1.x())), static_cast<std::size_t>(std::round(transformedVertex1.y()))};
+                        Math::Vector2S point2 = {static_cast<std::size_t>(std::round(transformedVertex2.x())), static_cast<std::size_t>(std::round(transformedVertex2.y()))};
+
+                        // TODO: Use fixed Image format here
+                        Rasterize::Triangle<Data::ImageFormat::RGB>(result, {point0, point1, point2}, {transformedVertex0.z(), transformedVertex1.z(), transformedVertex2.z()}, {uv0, uv1, uv2}, *pTexture, ratio, zBuffer);
+                    }
+                }
+            }
         }
     } // Rendering
 } // UltRenderer
