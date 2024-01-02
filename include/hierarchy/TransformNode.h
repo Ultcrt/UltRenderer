76a2d8d75eb8f58f30820f0ce4f18a8562cb1fb5
Ultcrt <zhang_zhe_yuan@outlook.com>
2024-01-02 16:01:35 +0800

    Implement a simple class for transform

---
+++ include/hierarchy/TransformNode.h
@@ -0,0 +1,18 @@
+//
+// Created by ultcrt on 24-1-2.
+//
+
+#ifndef ULTRENDERER_TRANSFORMNODE_H
+#define ULTRENDERER_TRANSFORMNODE_H
+
+#include "Node.h"
+
+namespace UltRenderer {
+    namespace Hierarchy {
+        class TransformNode: public Node {
+            Math::Transform3D transformMatrix;
+        };
+    } // Hierarchy
+} // UltRenderer
+
+#endif //ULTRENDERER_TRANSFORMNODE_H
---
+++ src/hierarchy/TransformNode.cpp
@@ -0,0 +1,10 @@
+//
+// Created by ultcrt on 24-1-2.
+//
+
+#include "hierarchy/TransformNode.h"
+
+namespace UltRenderer {
+    namespace Hierarchy {
+    } // Hierarchy
+} // UltRenderer
