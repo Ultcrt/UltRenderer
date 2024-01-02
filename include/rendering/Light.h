6e2305d6c4ee06fde56696e0c799807a0ad14b6f
Ultcrt <zhang_zhe_yuan@outlook.com>
2024-01-02 15:29:08 +0800

    Add a simple light class

---
+++ include/rendering/Light.h
@@ -0,0 +1,19 @@
+//
+// Created by ultcrt on 24-1-2.
+//
+
+#ifndef ULTRENDERER_LIGHT_H
+#define ULTRENDERER_LIGHT_H
+
+#include "math/Matrix.h"
+
+namespace UltRenderer {
+    namespace Rendering {
+        class Light {
+        public:
+            Math::Vector3D position;
+        };
+    } // Rendering
+} // UltRenderer
+
+#endif //ULTRENDERER_LIGHT_H
---
+++ src/rendering/Light.cpp
@@ -0,0 +1,10 @@
+//
+// Created by ultcrt on 24-1-2.
+//
+
+#include "rendering/Light.h"
+
+namespace UltRenderer {
+    namespace Rendering {
+    } // Rendering
+} // UltRenderer
