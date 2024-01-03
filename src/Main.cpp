b946eb18f3c7f8da5fd922662c1d9510b43be077
Ultcrt <zhang_zhe_yuan@outlook.com>
2024-01-04 00:03:01 +0800

    Implement simple test

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,47 +4,28 @@
 #include "rendering/Rasterize.h"
 #include "math/Matrix.h"
 #include "data/TriangleMesh.h"
+#include <memory>
+#include "rendering/Camera.h"
+#include "rendering/Light.h"
+#include "rendering/Scene.h"
 
 int main() {
-    UltRenderer::Data::Image texture("../data/african_head_diffuse.tga");
+    auto pTexture = std::make_shared<UltRenderer::Data::Image>("../data/african_head_diffuse.tga");
+    auto pMesh = std::make_shared<UltRenderer::Data::TriangleMesh>("../data/african_head.obj");
+    auto pCamera = std::make_shared<UltRenderer::Rendering::Camera>(1, 1);
+    auto pLight = std::make_shared<UltRenderer::Rendering::Light>(UltRenderer::Math::Vector3D{0, 0, 1});
 
-    UltRenderer::Data::Image img(1920, 1920, UltRenderer::Data::Pixel<UltRenderer::Data::ImageFormat::RGB>{0., 0., 0.});
+    pCamera->transformMatrix = UltRenderer::Math::Transform3D({1, 1, 1}, {0, 1.57, 0}, {1, 0, 0});
 
-    UltRenderer::Data::TriangleMesh model("../data/african_head.obj");
+    pMesh->pTexture = pTexture;
 
-    UltRenderer::Math::Vector3D light = {0, 0, -1};
+    UltRenderer::Rendering::Scene scene;
 
-    UltRenderer::Data::Image zBuffer(img.height(), img.width(), UltRenderer::Data::ImageFormat::GRAY);
+    scene.addCamera(pCamera);
+    scene.addMesh(pMesh);
+    scene.addLight(pLight);
 
-    for (const auto& tri: model.triangles) {
-        UltRenderer::Math::Vector3D vertex0 = model.vertices[tri[0]];
-        UltRenderer::Math::Vector3D vertex1 = model.vertices[tri[1]];
-        UltRenderer::Math::Vector3D vertex2 = model.vertices[tri[2]];
-
-        UltRenderer::Math::Vector3D uv0 = model.vertexTextures[tri[0]];
-        UltRenderer::Math::Vector3D uv1 = model.vertexTextures[tri[1]];
-        UltRenderer::Math::Vector3D uv2 = model.vertexTextures[tri[2]];
-
-        UltRenderer::Math::Vector2S point0 = {
-                static_cast<std::size_t>((vertex0.x() + 1) * static_cast<double>(img.shape().x() - 1) / 2),
-                static_cast<std::size_t>((vertex0.y() + 1) * static_cast<double>(img.shape().y() - 1) / 2)
-        };
-        UltRenderer::Math::Vector2S point1 = {
-                static_cast<std::size_t>((vertex1.x() + 1) * static_cast<double>(img.shape().x() - 1) / 2),
-                static_cast<std::size_t>((vertex1.y() + 1) * static_cast<double>(img.shape().y() - 1) / 2)
-        };
-        UltRenderer::Math::Vector2S point2 = {
-                static_cast<std::size_t>((vertex2.x() + 1) * static_cast<double>(img.shape().x() - 1) / 2),
-                static_cast<std::size_t>((vertex2.y() + 1) * static_cast<double>(img.shape().y() - 1) / 2)
-        };
-
-        UltRenderer::Math::Vector3D triangleNormal = ((vertex0 - vertex2).cross(vertex0 - vertex1)).normalized();
-        double ratio = triangleNormal.dot(light);
-
-        if (ratio > 0) {
-            UltRenderer::Rendering::Rasterize::Triangle<UltRenderer::Data::ImageFormat::RGB>(img, {point0, point1, point2}, {vertex0.z(), vertex1.z(), vertex2.z()}, {uv0, uv1, uv2}, texture, ratio, zBuffer);
-        }
-    }
+    auto img = pCamera->render<UltRenderer::Data::ImageFormat::RGB>(1920, 1920);
 
     img.save("test.tga");
 
