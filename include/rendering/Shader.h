33b27b2f209f95addc2c6838c865f4a3a1d3dd62
Ultcrt <zhang_zhe_yuan@outlook.com>
2024-01-06 23:02:53 +0800

    Implement Interpolator

--- include/rendering/Shader.h
+++ include/rendering/Shader.h
@@ -22,12 +22,35 @@ namespace UltRenderer {
             Math::Vector4D position;    // Works like gl_Position
         };
 
+        /**
+         * Base interpolator, user can derive or full specialize this class to implement interpolator for varying variables
+         * @tparam V A user defined struct would be interpolated by this class, need to be derived from Varying, works like varying in GLSL.
+         */
+        template <std::derived_from<Varying> V>
+        // Tips: Use concepts (c++20) to make V must be derived from Varying class
+        class Interpolator {
+            /**
+             * Callback function of interpolating triangle primitive
+             * @param varyings Comes from primitive assembly procedure, is a group of the vertex shader output in the same primitive.
+             * @param weights The weights used to interpolate in triangle primitive
+             * @return The interpolated V struct
+             */
+            virtual V operator()(const std::array<V, 3>& varyings, const Math::Vector3D& weights) const = 0;
+
+            /**
+             * Callback function of interpolating line primitive
+             * @param varyings Comes from primitive assembly procedure, is a group of the vertex shader output in the same primitive.
+             * @param weights The weights used to interpolate in line primitive
+             * @return The interpolated V struct
+             */
+            virtual V operator()(const std::array<V, 2>& varyings, const Math::Vector2D& weights) const = 0;
+        };
+
         /***
          * Base vertex shader, user can derive or full specialize this class to implement their own vertex shader
          * @tparam V A user defined struct returned by vertex shader, need to be derived from Varying, works like varying in GLSL.
          */
         template <std::derived_from<Varying> V>
-        // Tips: Use concepts (c++20) to make VARYING must be derived from Varying class
         class VertexShader {
         public:
             /**
@@ -35,7 +58,7 @@ namespace UltRenderer {
              * @param vertex The vertex need processing
              * @return A user defined struct derived from Varying
              */
-            virtual V operator()(const Math::Vector3D& vertex) = 0 const;
+            virtual V operator()(const Math::Vector3D& vertex) const = 0;
         };
 
         /**
@@ -51,7 +74,7 @@ namespace UltRenderer {
              * @param color The reference of color that need processing
              * @return Return true means color is processed, false means discarded
              */
-            virtual bool operator()(const V& varying, Math::Vector4D& color) = 0 const;
+            virtual bool operator()(const V& varying, Math::Vector4D& color) const = 0;
         };
     } // Rendering
 } // UltRender
