d051e19aa1d231cdd0a6f737ba0fc32c5536323a
Ultcrt <zhang_zhe_yuan@outlook.com>
2024-01-07 18:50:48 +0800

    Implement Pipeline which support vertex shader, fragment shader and some primitive procedures

---
+++ include/rendering/Pipeline.h
@@ -0,0 +1,65 @@
+//
+// Created by ultcrt on 24-1-6.
+//
+
+#ifndef ULTRENDERER_PIPELINE_H
+#define ULTRENDERER_PIPELINE_H
+
+#include <vector>
+#include <concepts>
+#include "math/Matrix.h"
+#include "data/Image.h"
+#include "shaders/IShader.h"
+#include "rendering/Rasterize.h"
+#include "math/Transform.h"
+
+namespace UltRenderer {
+    namespace Rendering {
+        class Pipeline {
+        public:
+            template <std::derived_from<Shaders::IVarying> V, std::derived_from<Shaders::IInterpolator<V>> IT, std::derived_from<Shaders::IVertexShader<V>> VS, std::derived_from<Shaders::IFragmentShader<V>> FS>
+            static void Execute(Data::Image& fBuffer, Data::Image& zBuffer, const Math::Transform3D& viewport, std::size_t vertexNum, const std::vector<Math::Vector3S>& triangles, const std::vector<Math::Vector2S>& lines, const std::vector<std::size_t>& points, const VS& vertexShader, const FS& fragmentShader, const IT& interpolator = {});
+        };
+
+        // TODO: Only support triangle primitives for now
+        template <std::derived_from<Shaders::IVarying> V, std::derived_from<Shaders::IInterpolator<V>> IT, std::derived_from<Shaders::IVertexShader<V>> VS, std::derived_from<Shaders::IFragmentShader<V>> FS>
+        void Pipeline::Execute(Data::Image& fBuffer, Data::Image& zBuffer, const Math::Transform3D& viewport, std::size_t vertexNum, const std::vector<Math::Vector3S>& triangles, const std::vector<Math::Vector2S>& lines, const std::vector<std::size_t>& points, const VS& vertexShader, const FS& fragmentShader, const IT& interpolator) {
+            // Process vertex
+            std::vector<V> varyings;
+            std::vector<bool> clipFlags;
+            for (std::size_t vIdx = 0; vIdx < vertexNum; vIdx++) {
+                // Call vertex shader for each vertex
+                V varying = vertexShader(vIdx);
+
+                // Record vertex clip flag here
+                const double w = std::abs(varying.position.w());
+                clipFlags.emplace_back(std::abs(varying.position.x()) > w || std::abs(varying.position.y()) > w || std::abs(varying.position.z()) > w);
+
+                // Apply viewport here
+                varying.position = viewport * varying.position / varying.position.w();
+                varyings.emplace_back(varying);
+            }
+
+            // Primitive assembly: triangle primitives
+            for (const auto& triangle: triangles) {
+                bool clipped = false;
+                std::array<V, 3> varyingGroup;
+                // TODO: Perspective correcting is not done, and clipping is not ideal (discard triangle when one vertex is outside)
+                for (std::size_t idx = 0; idx < 3; idx++) {
+                    varyingGroup[idx] = varyings[triangle[idx]];
+                    if (clipFlags[triangle[idx]]) {
+                        clipped = true;
+                        break;
+                    }
+                }
+
+                // Rasterize the primitive (fragment shader is also called)
+                if (!clipped) {
+                    Rendering::Rasterize::Triangle<V, IT, FS>(fBuffer, zBuffer, varyingGroup, fragmentShader, interpolator);
+                }
+            }
+        }
+    } // Rendering
+} // UltRenderer
+
+#endif //ULTRENDERER_PIPELINE_H
---
+++ src/rendering/Pipeline.cpp
@@ -0,0 +1,10 @@
+//
+// Created by ultcrt on 24-1-6.
+//
+
+#include "rendering/Pipeline.h"
+
+namespace UltRenderer {
+    namespace Rendering {
+    } // Rendering
+} // UltRenderer
