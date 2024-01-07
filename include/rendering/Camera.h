6a7ef56aacc444aa82152e0e3173d2691480e9a7
Ultcrt <zhang_zhe_yuan@outlook.com>
2024-01-07 18:51:57 +0800

    Integrate Pipeline to Camera (simple)

--- include/rendering/Camera.h
+++ include/rendering/Camera.h
@@ -11,6 +11,9 @@
 #include "hierarchy/TransformNode.h"
 #include "rendering/Scene.h"
 #include "rendering/Rasterize.h"
+#include "shaders/FlatShader.h"
+#include "rendering/Pipeline.h"
+#include "shaders/GouraudShader.h"
 
 namespace UltRenderer {
     namespace Rendering {
@@ -61,60 +64,28 @@ namespace UltRenderer {
             viewport(1, 3) = static_cast<double>(height) / 2.;
             viewport(2, 3) = 1. / 2.;
 
-            UltRenderer::Data::Image result(width, height, FORMAT);
+            UltRenderer::Data::Image fBuffer(width, height, Data::ImageFormat::RGBA);
             UltRenderer::Data::Image zBuffer(width, height, Data::ImageFormat::GRAY);
 
             // viewport * projection * view
-            const Math::Transform3D constTransform = viewport * projectionMatrix * transformMatrix.inverse();
             for (const auto& pMesh: _pScene->meshes()) {
-                const auto& pTexture = pMesh->pTexture;
-                const Math::Transform3D overallTransform = constTransform * pMesh->transformMatrix;
-                for (const auto& tri: pMesh->triangles) {
-                    UltRenderer::Math::Vector3D vertex0 = pMesh->vertices[tri[0]];
-                    UltRenderer::Math::Vector3D vertex1 = pMesh->vertices[tri[1]];
-                    UltRenderer::Math::Vector3D vertex2 = pMesh->vertices[tri[2]];
+                // TODO: Compute only the first light here for simplicity, which is wrong
+                Math::Vector3D light = _pScene->lights()[0]->position;
 
-                    UltRenderer::Math::Vector3D transformedVertex0 = (overallTransform * vertex0.toHomogeneousCoordinates(1)).toCartesianCoordinates();
-                    UltRenderer::Math::Vector3D transformedVertex1 = (overallTransform * vertex1.toHomogeneousCoordinates(1)).toCartesianCoordinates();
-                    UltRenderer::Math::Vector3D transformedVertex2 = (overallTransform * vertex2.toHomogeneousCoordinates(1)).toCartesianCoordinates();
+                // TODO: Should pass by Scene
+                Shaders::GouraudVertexShader vertexShader(light, pMesh->vertices, pMesh->vertexNormals, pMesh->vertexTextures, pMesh->transformMatrix, transformMatrix.inverse(), projectionMatrix);
+                Shaders::GouraudFragmentShader fragmentShader(*pMesh->pTexture);
 
-                    // TODO: Need to support texture-less mesh
-                    UltRenderer::Math::Vector3D uv0 = pMesh->vertexTextures[tri[0]];
-                    UltRenderer::Math::Vector3D uv1 = pMesh->vertexTextures[tri[1]];
-                    UltRenderer::Math::Vector3D uv2 = pMesh->vertexTextures[tri[2]];
+                Pipeline::Execute<Shaders::GouraudVarying, Shaders::GouraudInterpolator, Shaders::GouraudVertexShader, Shaders::GouraudFragmentShader>(fBuffer, zBuffer, viewport, pMesh->vertices.size(), pMesh->triangles, {}, {}, vertexShader, fragmentShader);
 
-                    Math::Vector3D avgLight;
-                    // TODO: Compute Average light here for simplicity, which is wrong
-                    for (const auto& light: _pScene->lights()) {
-                        avgLight = avgLight + light->position;
-                    }
-                    UltRenderer::Math::Vector3D triangleNormal = ((vertex0 - vertex2).cross(vertex0 - vertex1)).normalized();
-                    double ratio = triangleNormal.dot(avgLight.normalized());
+//                Shaders::FlatVertexShader vertexShader(pMesh->vertices, pMesh->vertexNormals, pMesh->vertexTextures, pMesh->transformMatrix, transformMatrix.inverse(), projectionMatrix);
+//                Shaders::FlatFragmentShader fragmentShader(*pMesh->pTexture, light);
+//
+//                Pipeline::Execute<Shaders::FlatVarying, Shaders::FlatInterpolator, Shaders::FlatVertexShader, Shaders::FlatFragmentShader>(fBuffer, zBuffer, viewport, pMesh->vertices.size(), pMesh->triangles, {}, {}, vertexShader, fragmentShader);
 
-                    if (ratio > 0) {
-                        // TODO: Discard triangle when one vertex is outside image
-                        if (transformedVertex0.x() >= 0 && transformedVertex0.x() <= static_cast<double>(width)) {
-                            if (transformedVertex1.x() >= 0 && transformedVertex1.x() <= static_cast<double>(width)) {
-                                if (transformedVertex2.x() >= 0 && transformedVertex2.x() <= static_cast<double>(width)) {
-                                    if (transformedVertex0.y() >= 0 && transformedVertex0.y() <= static_cast<double>(height)) {
-                                        if (transformedVertex1.y() >= 0 && transformedVertex1.y() <= static_cast<double>(height)) {
-                                            if (transformedVertex2.y() >= 0 && transformedVertex2.y() <= static_cast<double>(height)) {
-                                                Math::Vector2S point0 = {static_cast<std::size_t>(std::round(transformedVertex0.x())), static_cast<std::size_t>(std::round(transformedVertex0.y()))};
-                                                Math::Vector2S point1 = {static_cast<std::size_t>(std::round(transformedVertex1.x())), static_cast<std::size_t>(std::round(transformedVertex1.y()))};
-                                                Math::Vector2S point2 = {static_cast<std::size_t>(std::round(transformedVertex2.x())), static_cast<std::size_t>(std::round(transformedVertex2.y()))};
-
-                                                Rasterize::Triangle<FORMAT>(result, {point0, point1, point2}, {transformedVertex0.z(), transformedVertex1.z(), transformedVertex2.z()}, {uv0, uv1, uv2}, *pTexture, ratio, zBuffer);
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
             }
 
-            return result;
+            return fBuffer;
         }
     } // Rendering
 } // UltRenderer
