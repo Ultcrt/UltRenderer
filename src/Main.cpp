6bcc5d123b736f682a2f6a2b50d12c2c1044b75d
Ultcrt <zhang_zhe_yuan@outlook.com>
2024-01-08 21:40:12 +0800

    Update test

--- src/Main.cpp
+++ src/Main.cpp
@@ -12,11 +12,11 @@
 int main() {
     auto pTexture = std::make_shared<UltRenderer::Data::Image>("../data/african_head_diffuse.tga");
     auto pMesh = std::make_shared<UltRenderer::Data::TriangleMesh>("../data/african_head.obj");
-    auto pCamera = std::make_shared<UltRenderer::Rendering::Camera>(2, 2, 0.1, 10);
+    auto pCamera = std::make_shared<UltRenderer::Rendering::Camera>(2, 2, 4, 10);
     auto pLight = std::make_shared<UltRenderer::Rendering::Light>(UltRenderer::Math::Vector3D{0, 0, -1});
 
     // Use 3.14 and a large z, will cause head not at center
-    pCamera->transformMatrix = UltRenderer::Math::Transform3D({1, 1, 1}, {0, 0, 0}, {0, 0, 1});
+    pCamera->transformMatrix = UltRenderer::Math::Transform3D({1, 1, 1}, {0, M_PI / 8, -M_PI / 10}, {2.1, 1.8, 5});
 
     pMesh->pTexture = pTexture;
 
