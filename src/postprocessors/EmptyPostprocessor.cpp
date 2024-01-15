7b6fa2be1f891679b846fe02052dd665ae4c0cbb
Ultcrt <zhang_zhe_yuan@outlook.com>
2024-01-15 14:40:07 +0800

    Implement an empty postprocessor (for default argument)

---
+++ src/postprocessors/EmptyPostprocessor.cpp
@@ -0,0 +1,12 @@
+//
+// Created by ultcrt on 24-1-15.
+//
+
+#include "postprocessors/EmptyPostprocessor.h"
+
+namespace UltRenderer {
+    namespace Postprocessors {
+        void EmptyPostprocessor::operator()(Data::Image &fBuffer, Data::Image &zBuffer) {
+        }
+    } // Postprocessors
+} // UltRenderer
