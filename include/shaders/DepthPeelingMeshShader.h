0370d7dca7e7a800086e723b74d8095657f87dd6
Ultcrt <zhang_zhe_yuan@outlook.com>
2024-01-21 15:12:43 +0800

    Implement depth peeling shader

---
+++ include/shaders/DepthPeelingMeshShader.h
@@ -0,0 +1,21 @@
+//
+// Created by ultcrt on 24-1-21.
+//
+
+#ifndef ULTRENDERER_DEPTHPEELINGMESHSHADER_H
+#define ULTRENDERER_DEPTHPEELINGMESHSHADER_H
+
+#include "IMeshShader.h"
+
+namespace UltRenderer {
+    namespace Shaders {
+        class DepthPeelingMeshFragmentShader: public IMeshFragmentShader<IMeshVarying> {
+        public:
+            const Data::Image* pLastDepthLayer;
+
+            bool operator()(const IMeshVarying& varying, const Math::Vector4D& fragCoord, Math::Vector4D& color, double& depth) const override;
+        };
+    } // Shaders
+} // UltRenderer
+
+#endif //ULTRENDERER_DEPTHPEELINGMESHSHADER_H
---
+++ src/shaders/DepthPeelingMeshShader.cpp
@@ -0,0 +1,21 @@
+//
+// Created by ultcrt on 24-1-21.
+//
+
+#include <iostream>
+#include "shaders/DepthPeelingMeshShader.h"
+
+namespace UltRenderer {
+    namespace Shaders {
+        bool DepthPeelingMeshFragmentShader::operator()(const IMeshVarying &varying, const Math::Vector4D& fragCoord, Math::Vector4D &color,
+                                                 double &depth) const {
+            // Discard when depth is equals to last depth layer, to make last layer would not be right into zBuffer
+            double lastDepth = (*pLastDepthLayer).at<Data::ImageFormat::GRAY>(static_cast<std::size_t>(fragCoord.x()), static_cast<std::size_t>(fragCoord.y()))[0];
+            if (lastDepth >= depth) {
+                return false;
+            }
+
+            return true;
+        }
+    } // Shaders
+} // UltRenderer
