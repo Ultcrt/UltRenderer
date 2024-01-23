9d97acd12e9d8925741b522689b39f150b27024d
Ultcrt <zhang_zhe_yuan@outlook.com>
2024-01-23 17:54:50 +0800

    Transform fragCoord into camera frame

--- src/rendering/raytracing/Camera.cpp
+++ src/rendering/raytracing/Camera.cpp
@@ -12,7 +12,22 @@ namespace UltRenderer {
             }
 
             Data::Image Rendering::Raytracing::Camera::render(std::size_t width, std::size_t height) const {
-                return Data::Image(0, 0, Data::ImageFormat::RGBA);
+                Data::Image img(width, height, Data::ImageFormat::RGBA);
+
+                const Math::Transform3D view = transformMatrix.inverse();
+
+                // Raytracing is done under camera frame
+                const Math::Vector3D origin = {0, 0, 0};
+
+                for (std::size_t w = 0; w < width; w++) {
+                    for (std::size_t h = 0; h < height; h++) {
+                        const auto direction = Math::Vector3D {
+                            ((static_cast<double>(w) + 0.5) / static_cast<double>(width)) * _width - _width / 2,
+                            ((static_cast<double>(h) + 0.5) / static_cast<double>(height)) * _height - _height / 2,
+                            -_zMax
+                        }.normalized();
+                    }
+                }
             }
         } // Raytracing
     } // Rendering
