5dc17e3566942193b87dd6c20420043af586277d
Ultcrt <zhang_zhe_yuan@outlook.com>
2024-01-24 15:44:28 +0800

    Update test

--- src/Main.cpp
+++ src/Main.cpp
@@ -29,36 +29,53 @@ int main() {
     fs.shadowIntensity = 0.3;
     fs.glowIntensity = 3.0;
 
+    // Obj
     auto pTexture = std::make_shared<Data::Image>("../data/diablo3_pose_diffuse.tga", Data::FilterType::LINEAR);
     auto pNormalMap = std::make_shared<Data::Image>("../data/diablo3_pose_nm_tangent.tga", Data::FilterType::LINEAR);
     auto pSpecular = std::make_shared<Data::Image>("../data/diablo3_pose_spec.tga", Data::FilterType::LINEAR);
     auto pGlowMap = std::make_shared<Data::Image>("../data/diablo3_pose_glow.tga", Data::FilterType::LINEAR);
     auto pMesh = std::make_shared<Data::TriangleMesh>("../data/diablo3_pose.obj");
-    auto pRasterizingCamera = std::make_shared<Rendering::Rasterizing::Camera>(2, 2, 4);
-    auto pRaytracingCamera = std::make_shared<Rendering::Raytracing::Camera>(2, 2, 4);
-    auto pLight = std::make_shared<Rendering::Light>(Math::Vector3D{0, 0, -1}, 2);
-
-    // Use 3.14 and a large z, will cause head not at center
-    pRasterizingCamera->transformMatrix = Math::Transform3D::FromLookAt({2, 3, 5}, {0, 0, 0}, Math::Vector3D::Y());
-    pRaytracingCamera->transformMatrix = Math::Transform3D::FromLookAt({2, 3, 5}, {0, 0, 0}, Math::Vector3D::Y());
 
     pMesh->pTexture = pTexture;
     pMesh->pNormalMap = pNormalMap;
-    pMesh->pSpecular = pSpecular;
+    pMesh->pSpecularMap = pSpecular;
     pMesh->normalMapType = Data::NormalMapType::DARBOUX;
     pMesh->pGlowMap = pGlowMap;
 
+    // Floor
+    auto pFloorTexture = std::make_shared<Data::Image>("../data/floor_diffuse.tga", Data::FilterType::LINEAR);
+    auto pFloorNormalMap = std::make_shared<Data::Image>("../data/floor_nm_tangent.tga", Data::FilterType::LINEAR);
+    auto pFloorMesh = std::make_shared<Data::TriangleMesh>("../data/floor.obj");
+
+    pFloorMesh->setTexture(pFloorTexture);
+    pFloorMesh->pNormalMap = pFloorNormalMap;
+    pFloorMesh->normalMapType = Data::NormalMapType::DARBOUX;
+
+    // Camera
+    auto pRasterizingCamera = std::make_shared<Rendering::Rasterizing::Camera>(2, 2, 4);
+    auto pRaytracingCamera = std::make_shared<Rendering::Raytracing::Camera>(2, 2, 4);
+
+    pRasterizingCamera->transformMatrix = Math::Transform3D::FromLookAt({2, 3, 5}, {0, 0, 0}, Math::Vector3D::Y());
+    pRaytracingCamera->transformMatrix = Math::Transform3D::FromLookAt({2, 3, 5}, {0, 0, 0}, Math::Vector3D::Y());
+
+    // Light
+    auto pLight = std::make_shared<Rendering::Light>(Math::Vector3D{0, 0, -1}, 2);
+
+    // Scene
     Rendering::Scene scene;
 
+    scene.addMesh(pMesh);
+    scene.addMesh(pFloorMesh);
     scene.addCamera(pRasterizingCamera);
     scene.addCamera(pRaytracingCamera);
-    scene.addMesh(pMesh);
     scene.addLight(pLight);
 
-    auto imgRasterizing = pRasterizingCamera->render(128, 128, vs, fs, it);
-    auto imgRaytracing = pRaytracingCamera->render(128, 128);
+    // Rendering
+//    auto imgRasterizing = pRasterizingCamera->render(128, 128, vs, fs, it);
+    auto imgRaytracing = pRaytracingCamera->render(800, 800);
 
-    imgRasterizing.save("rasterizing.tga");
+    // Saving
+//    imgRasterizing.save("rasterizing.tga");
     imgRaytracing.save("raytracing.tga");
 
     return 0;
