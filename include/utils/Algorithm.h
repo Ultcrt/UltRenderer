60fcf593099c26b0cf7d45d3b82d0f42d81c3791
Ultcrt <zhang_zhe_yuan@outlook.com>
2024-01-28 14:07:55 +0800

    Implement ArgMin ArgMax

--- include/utils/Algorithm.h
+++ include/utils/Algorithm.h
@@ -11,16 +11,38 @@
 
 namespace UltRenderer {
     namespace Utils {
+        /*----------Declaration----------*/
         // Tips: Any type that can match the signature: bool (const T& a, const T& b). Because template is parsed at compile time.
         template <typename T, typename Compare=std::less<T>>
-        std::vector<std::size_t> ArgSort(const std::vector<T>& target, Compare comp);
+        std::vector<std::size_t> ArgSort(const std::vector<T>& target, Compare comp = {});
 
+        template <typename T, typename Compare=std::less<T>>
+        std::size_t ArgMax(const std::vector<T>& target, Compare comp = {});
+
+        template <typename T, typename Compare=std::less<T>>
+        std::size_t ArgMin(const std::vector<T>& target, Compare comp = {});
+
+        /*----------Definition----------*/
         template <typename T, typename Compare>
         std::vector<std::size_t> ArgSort(const std::vector<T>& target, Compare comp) {
             std::vector<std::size_t> indices(target.size());
             std::iota(std::begin(indices), std::end(indices), 0);
             std::stable_sort(indices.begin(), indices.end(), [&target, &comp](std::size_t a, std::size_t b){return comp(target[a], target[b]);});
             return indices;
+        }
+
+        template <typename T, typename Compare>
+        std::size_t ArgMax(const std::vector<T>& target, Compare comp) {
+            std::vector<std::size_t> indices(target.size());
+            std::iota(std::begin(indices), std::end(indices), 0);
+            return *std::max_element(indices.begin(), indices.end(), [&target, &comp](std::size_t a, std::size_t b){return comp(target[a], target[b]);});
+        }
+
+        template <typename T, typename Compare>
+        std::size_t ArgMin(const std::vector<T>& target, Compare comp) {
+            std::vector<std::size_t> indices(target.size());
+            std::iota(std::begin(indices), std::end(indices), 0);
+            return *std::min_element(indices.begin(), indices.end(), [&target, &comp](std::size_t a, std::size_t b){return comp(target[a], target[b]);});
         }
     } // Utils
 } // UltRenderer
