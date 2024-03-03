c719626dafa7bda49f70feecdaf7bf6c4b405829
Ultcrt <zhang_zhe_yuan@outlook.com>
2024-03-03 15:09:53 +0800

    Implement Disney brdf

---
+++ include/rendering/material/DisneyBRDFMaterial.h
@@ -0,0 +1,22 @@
+//
+// Created by ultcrt on 24-3-3.
+//
+
+#ifndef ULTRENDERER_DISNEYBRDFMATERIAL_H
+#define ULTRENDERER_DISNEYBRDFMATERIAL_H
+
+#include "CommonMaterial.h"
+
+namespace UltRenderer {
+    namespace Rendering {
+        namespace Material {
+            class DisneyBRDFMaterial: public CommonMaterial {
+            public:
+                double roughness;
+                Math::Vector3D getBSDF(const Math::Vector3D &uv, const Math::Vector3D &normal, const Math::Vector3D &view, const Math::Vector3D &light) const override;
+            };
+        } // Material
+    } // Rendering
+} // UltRenderer
+
+#endif //ULTRENDERER_DISNEYBRDFMATERIAL_H
---
+++ src/rendering/material/DisneyBRDFMaterial.cpp
@@ -0,0 +1,20 @@
+//
+// Created by ultcrt on 24-3-3.
+//
+
+#include "rendering/material/DisneyBRDFMaterial.h"
+
+namespace UltRenderer {
+    namespace Rendering {
+        namespace Material {
+            Math::Vector3D DisneyBRDFMaterial::getBSDF(const Math::Vector3D &uv, const Math::Vector3D &normal,
+                                                       const Math::Vector3D &view, const Math::Vector3D &light) const {
+                Math::Vector3D half = (view - light) / 2;
+                double cosThetaV = view.dot(normal);
+                double cosThetaL = -light.dot(normal);
+                double fD90 = 0.5 + 2 * roughness * std::pow(half.dot(-light), 2.);
+                return getDiffuseColor(uv) / M_PI * (1 + (fD90 - 1) * std::pow(1. - cosThetaL, 5.)) * (1 + (fD90 - 1) * std::pow(1. - cosThetaV, 5.));
+            }
+        } // Material
+    } // Rendering
+} // UltRenderer
