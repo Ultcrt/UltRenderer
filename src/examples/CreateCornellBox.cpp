daeb7ef5874e52f05184ad0d73f5f5c724428b8d
Ultcrt <zhang_zhe_yuan@outlook.com>
2024-03-05 00:08:45 +0800

    Implement Cornell box scene

--- src/examples/CreateCornellBox.cpp
+++ src/examples/CreateCornellBox.cpp
@@ -7,14 +7,20 @@
 #include "hierarchy/IntersectableNode.h"
 #include "rendering/Scene.h"
 #include "rendering/raytracing/Camera.h"
+#include "rendering/material/LambertianBRDFMaterial.h"
+#include "rendering/light/DirectionalLight.h"
+#include "rendering/light/PointLight.h"
+#include "rendering/light/PlaneLight.h"
 
 using namespace UltRenderer;
 using namespace std;
 
-inline unique_ptr<Rendering::Scene> CreateCornellBox() {
+inline unique_ptr<Rendering::Scene> CreateCornellBox(const std::shared_ptr<Rendering::ICamera>& pCamera) {
     auto pScene = std::make_unique<Rendering::Scene>();
 
-    auto pCamera = make_shared<Rendering::Raytracing::Camera>(0.025, 0.035, 0.025);
+    pCamera->setWidth(0.025);
+    pCamera->setHeight(0.025);
+    pCamera->setZMin(0.035);
     pCamera->transformMatrix = Math::Transform3D::FromLookAt(
             {278.0, 273.0, -800.0},
             {278.0, 273.0, -799.0},
@@ -26,13 +32,6 @@ inline unique_ptr<Rendering::Scene> CreateCornellBox() {
             {0.0,   0.0, 0.0},
             {0.0,   0.0, 559.2},
             {549.6, 0.0, 559.2},
-    };
-
-    vector<Math::Vector3D> lightVertices = {
-            {343.0, 548.8, 227.0},
-            {343.0, 548.8, 332.0},
-            {213.0, 548.8, 332.0},
-            {213.0, 548.8, 227.0},
     };
 
     vector<Math::Vector3D> ceilVertices = {
@@ -140,24 +139,53 @@ inline unique_ptr<Rendering::Scene> CreateCornellBox() {
     };
 
     auto pFloor = make_shared<Data::TriangleMesh>(floorVertices, planeTriangles);
-    auto pLight = make_shared<Data::TriangleMesh>(lightVertices, planeTriangles);
     auto pCeil = make_shared<Data::TriangleMesh>(ceilVertices, planeTriangles);
     auto pBack = make_shared<Data::TriangleMesh>(backVertices, planeTriangles);
     auto pRight = make_shared<Data::TriangleMesh>(rightVertices, planeTriangles);
     auto pLeft = make_shared<Data::TriangleMesh>(leftVertices, planeTriangles);
 
-    auto pShort = make_shared<Data::TriangleMesh>(shortVertices, planeTriangles);
-    auto pTall = make_shared<Data::TriangleMesh>(tallVertices, planeTriangles);
+    auto pShort = make_shared<Data::TriangleMesh>(shortVertices, blockTriangles);
+    auto pTall = make_shared<Data::TriangleMesh>(tallVertices, blockTriangles);
+
+    pFloor->pMaterial = std::make_shared<Rendering::Material::LambertianBRDFMaterial>();
+    pFloor->pMaterial->diffuseColor = {1, 1, 1};
+
+    pCeil->pMaterial = std::make_shared<Rendering::Material::LambertianBRDFMaterial>();
+    pCeil->pMaterial->diffuseColor = {1, 1, 1};
+
+    pBack->pMaterial = std::make_shared<Rendering::Material::LambertianBRDFMaterial>();
+    pBack->pMaterial->diffuseColor = {1, 1, 1};
+
+    pRight->pMaterial = std::make_shared<Rendering::Material::LambertianBRDFMaterial>();
+    pRight->pMaterial->diffuseColor = {0, 1, 0};
+
+    pLeft->pMaterial = std::make_shared<Rendering::Material::LambertianBRDFMaterial>();
+    pLeft->pMaterial->diffuseColor = {1, 0, 0};
+
+    pShort->pMaterial = std::make_shared<Rendering::Material::LambertianBRDFMaterial>();
+    pShort->pMaterial->diffuseColor = {1, 1, 1};
+
+    pTall->pMaterial = std::make_shared<Rendering::Material::LambertianBRDFMaterial>();
+    pTall->pMaterial->diffuseColor = {1, 1, 1};
+
 
     pScene->addCamera(pCamera);
     pScene->addMesh(pFloor);
-    pScene->addMesh(pLight);
     pScene->addMesh(pCeil);
     pScene->addMesh(pBack);
     pScene->addMesh(pRight);
     pScene->addMesh(pLeft);
     pScene->addMesh(pShort);
     pScene->addMesh(pTall);
+
+    auto pLight = make_shared<Rendering::Light::PlaneLight>(130, 105, 100);
+    pLight->transformMatrix = {
+            1., 0., 0., 278,
+            0., -1., 0., 548.8,
+            0., 0., 1., 279.5,
+            0., 0., 0., 1.,
+    };
+    pScene->addAreaLight(pLight);
 
     return pScene;
 }
