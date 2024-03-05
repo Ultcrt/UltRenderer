d2c40887622c5b09a20adbc76a290d483865ca41
Ultcrt <zhang_zhe_yuan@outlook.com>
2024-03-05 18:10:38 +0800

    Fix Labertian evaluating

--- src/rendering/BSDF.cpp
+++ src/rendering/BSDF.cpp
@@ -10,7 +10,7 @@ namespace UltRenderer {
             Math::Vector3D LambertianDiffuseBRDF(const Math::Vector3D& uv, const Material::CommonMaterial& mat) {
                 Math::Vector3D color = mat.getDiffuseColor(uv);
 
-                return color * mat.diffuseCoefficient / M_PI;
+                return color / M_PI;
             }
 
             Math::Vector3D PhongReflectionBRDF(const Math::Vector3D& uv, const Material::CommonMaterial& mat) {
